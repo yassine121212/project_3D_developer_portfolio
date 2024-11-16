91265f93a0efcb7449774cb54afb6866bdabf318
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2024-11-16 10:08:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 import "./index.css";
 
